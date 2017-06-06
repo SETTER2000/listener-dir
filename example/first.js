f45ea5be7d94f13e2bab7a2fc14e126442fc16a5
SETTER <info@lphp.ru>
2017-06-06 17:01:24 +0300

    Refactoring modules and added new functional

--- example/first.js
+++ example/first.js
@@ -7,23 +7,22 @@
  * Date: 01.06.2017
  * Time: 22:55
  */
-const fs = require('fs');
+
 const Watcher = require('../listener-dir');
+const source = 'Z:/';
+const target = 'done';
 
 // После определения класса Watcher можно воспользоваться им, создав объект Watcher
-var watcher = new Watcher('watch', 'done');
+var watcher = new Watcher(source, target);
 
 /**
  * В только что созданном объекте Watcher можно использовать метод on,
  * унаследованный от класса генератора событий, чтобы создать
  * логику обработки каждого файла,
+ * Метод mov перемещает файлы из папки source в папку target
  */
 watcher.on('process', function process(file) {
-    var watchFile = this.watchDir + '/' + file;
-    var processedFile = this.processedDir + '/' + file.toLowerCase();
-    fs.rename(watchFile, processedFile, (err)=> {
-        if (err) throw err;
-    });
+    watcher.mov(file);
 });
 
 /**
--- listener-dir.js
+++ listener-dir.js
@@ -7,7 +7,7 @@ function Watcher(watchDir, processedDir) {
 var events = require('events');
 var fs = require('fs');
 var util = require('util');
-
+var mv = require('mv');
 /**
  * Использование функции inherits, которая является
  * частью встроенного в Node модуля util. С помощью функции inherits
@@ -19,13 +19,22 @@ util.inherits(Watcher, events.EventEmitter);
 
 // Расширение возможностей класса EventEmitter путем добавления метода обработки файлов
 Watcher.prototype.watch = function () {
-    fs.readdir(this.watchDir, (err, files)=> {
-        if (err) throw err;
+    fs.readdir(this.watchDir, {'encoding':'utf-8'},(err, files)=> {
+        if (err) console.log('Ошибка! Не могу читать директорию: '+ err);
         for (var index in files) {
-// Обработка каждого файла в заданной папке
-            this.emit('process', files[index]);
+          if(files[index] != 'Forms'){
+              // Обработка каждого файла в заданной папке
+              this.emit('process', files[index]);
+          }
         }
     })
+};
+Watcher.prototype.mov = function (file) {
+    "use strict";
+    mv(this.watchDir + '/' + file, this.processedDir + '/' + file, function(err) {
+        if (err)console.log(err);
+        console.log('file moved successfully');
+    });
 };
 // Расширение класса EventEmitter путем добавления метода start, инициирующего просмотр
 Watcher.prototype.start = function () {
